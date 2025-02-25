c81c2a88fa3068e78b3e8d1423808e851200aced
Gabriel Solorzano <Gabosolorzano17@gmail.com>
2025-02-24 18:12:57 -0600

    v3

--- src/components/sections/Habilidades.jsx
+++ src/components/sections/Habilidades.jsx
@@ -1,26 +1,51 @@
+import datos from "../../mocks/datos.json";
+import { LanguageContext } from "../../context/LanguageProvider";
+import { useContext } from "react";
+
 export const Habilidades = () => {
+  const { selectedLanguage } = useContext(LanguageContext);
+  const skills = datos[selectedLanguage].skills;
+  console.log(skills);
+
+  const filters = skills.content.filter((datos) => datos.area === "Frontend");
+  console.log(filters);
+
   return (
     <>
-      <main className=" w-full h-full p-4 gap-4 grid grid-cols-2">
-        <div className="border-4  border-secondary rounded-2xl shadow-retro 
-                   dark:border-primary dark:shadow-retroDark"
-        >
-          <header className=" flex items-center p-7 border-b-4  h-10 border-secondary dark:border-primary">
-            <div className="w-8 h-8 bg-secondary rounded-full dark:bg-primary "></div>
-            <span className="flex-1 text-center font-title text-7xl">Frontend</span>
-          </header>
-          <ul className="grid grid-cols-4 h-[calc(100%-44px)] p-2 gap-4 ">
-            <li className="inline-block h-14 bg-secondary text-primary px-4 py-2 rounded-full font-body text-2xl 
-                       dark:bg-primary dark:text-secondary"
+      {skills ? (
+        <main className=" w-full h-full p-4 gap-4 grid grid-cols-2">
+          {skills.categories.map((datos) => (
+            <div
+              key={datos.id}
+              className="border-4  border-secondary rounded-2xl shadow-retro 
+                     dark:border-primary dark:shadow-retroDark"
             >
-              <i className="fa-brands fa-linkedin fa-lg mr-2"></i>
-              <span>Linkedin</span>
-            </li>
-          </ul>
-        </div>
-        
-
-      </main>
+              <header className=" flex items-center p-7 border-b-4  h-10 border-secondary dark:border-primary">
+                <div className="w-8 h-8 bg-secondary rounded-full dark:bg-primary "></div>
+                <span className="flex-1 text-center font-title text-7xl">
+                  {datos.name}
+                </span>
+              </header>
+              <ul className="grid grid-cols-4 h-[calc(100%-44px)] p-2 gap-4 ">
+                {skills.content
+                  .filter((skill) => skill.area === datos.name)
+                  .map((dato) => (
+                    <li
+                      key={dato.id}
+                      className="flex  gap-2 justify-center items-center h-14 bg-secondary text-primary px-4 py-2 rounded-full font-body text-[1.2rem] 
+                     dark:bg-primary dark:text-secondary"
+                    >
+                      <i className="fa-brands fa-linkedin fa-lg mr-2"></i>
+                      <span>{dato.skill}</span>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          ))}
+        </main>
+      ) : (
+        <p>No hay datos</p>
+      )}
     </>
   );
 };
